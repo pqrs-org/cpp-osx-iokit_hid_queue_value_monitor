cba02b6217587c8570157a24805fbbcb795f8ba2
Takayama Fumihiko <tekezo@pqrs.org>
2019-05-19 23:38:55 +0900

    version 1.10.0

--- include/pqrs/osx/iokit_hid_queue_value_monitor.hpp
+++ include/pqrs/osx/iokit_hid_queue_value_monitor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::iokit_hid_queue_value_monitor v1.9
+// pqrs::iokit_hid_queue_value_monitor v1.10
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
